648ee49e8872702d69bca76ef726dd0f0bfa8758
samzthedesigner-hue <samzthedesigner@gmail.com>
2026-07-15 11:54:46 +0100

    Update Vite config base path and output directory
    Change the base path for the Vite configuration and specify the output directory for the build.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/torch/',
+  base: '/',
+  build: {
+    outDir: 'dist'
+  },
   worker: {
     format: 'es'
   }
